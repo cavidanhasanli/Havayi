20074a3c93521b6402b2c82de7f41d1a3cca22f0
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-12-11 13:44:14 +0400

    admin customize

--- dashboard_app/admin.py
+++ dashboard_app/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from dashboard_app.models import *
-from dashboard_app.models import BankList
+from dashboard_app.models import BankList, BlankCredit
 from dashboard_app.signals import bank_send_mail
 
 
 class BlankCreditAdmin(admin.ModelAdmin):
-    list_display = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id',)
+    list_display = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'bank_id',)
     list_display_links = None
-    list_editable = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
+    list_editable = ('bank_id',)
     list_filter = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
     change_form_template = 'admin/change_list.html'
     actions = ['send_invite']
+
+    def has_add_permission(self, request):
+        return False
 
     def send_invite(self, request, queryset):
         for email in queryset:
@@ -19,6 +22,13 @@ class BlankCreditAdmin(admin.ModelAdmin):
     send_invite.short_description = "Send invitation"
 
 
-admin.site.register(BankList)
+class BankListAdmin(admin.ModelAdmin):
+    list_display = ('bank_name', 'bank_email', 'credit_type', 'interest',)
+    list_display_links = None
+    list_editable = ('bank_name', 'bank_email', 'credit_type', 'interest',)
+    list_filter = ('bank_name', 'bank_email', 'credit_type', 'interest',)
+
+
+admin.site.register(BankList, BankListAdmin)
 admin.site.register(BlankCredit, BlankCreditAdmin)
 admin.site.register(CreditFields)
--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user_app.models import MyUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -15,6 +16,8 @@ class CreditFields(models.Model):
 class BankList(models.Model):
     bank_name = models.CharField(max_length=100, null=True, blank=True)
     bank_email = models.EmailField(null=True, blank=True)
+    credit_type = models.ForeignKey(CreditFields,on_delete=models.CASCADE,null=True,blank=True)
+    interest = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100)], default=0.0,null=True,blank=True)
 
     def __str__(self):
         return self.bank_name
@@ -30,6 +33,7 @@ class BlankCredit(models.Model):
     send_user_num = models.IntegerField()
     otp_status = models.BooleanField("otp status", default=False)
     bank_id = models.ForeignKey(BankList, on_delete=models.CASCADE, null=True, blank=True)
+
     # file_field = models.FileField(upload_to='user_files',null=True,blank=True)
 
     def __str__(self):
