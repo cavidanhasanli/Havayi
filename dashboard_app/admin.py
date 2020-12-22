5c8687e4f3b0a136d9414518d7d1be4eceaba321
cavidanhasanli <cavidan.hasanli@mail.ru>
2020-12-22 13:41:09 +0400

    admin change

--- dashboard_app/admin.py
+++ dashboard_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from dashboard_app.models import *
-from dashboard_app.models import BankList, BlankCredit
+from dashboard_app.models import BankList, BlankCredit, SendedBlank
 from dashboard_app.signals import bank_send_mail
 
 
 class BlankCreditAdmin(admin.ModelAdmin):
-    list_display = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num','otp_status', 'bank_id')
+    list_display = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
     list_display_links = None
     list_editable = ('bank_id',)
     list_filter = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
@@ -27,8 +27,20 @@ class BlankCreditAdmin(admin.ModelAdmin):
 
     def send_invite(self, request, queryset):
         for email in queryset:
-            print(email)
+            print(email.phone_number)
             bank_send_mail(sender=None, instance=email, created=None)
+            SendedBlank.objects.create(
+                user_id=email.user_id,
+                name_surname=email.name_surname,
+                phone_number=email.phone_number,
+                amount=email.amount,
+                credit_type=email.credit_type,
+                date=email.date,
+                send_user_num=email.send_user_num,
+                otp_status=email.otp_status,
+                bank_id=email.bank_id
+            )
+            queryset.delete()
 
     send_invite.short_description = "Send invitation"
 
@@ -40,6 +52,13 @@ class BankListAdmin(admin.ModelAdmin):
     list_filter = ('bank_name', 'bank_email', 'credit_type', 'interest',)
 
 
+class SendedBankAdmin(admin.ModelAdmin):
+    list_display = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
+    list_display_links = None
+    list_filter = ('name_surname', 'phone_number', 'amount', 'credit_type', 'send_user_num', 'otp_status', 'bank_id')
+
+
 admin.site.register(BankList, BankListAdmin)
 admin.site.register(BlankCredit, BlankCreditAdmin)
 admin.site.register(CreditFields)
+admin.site.register(SendedBlank, SendedBankAdmin)
--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -16,8 +16,9 @@ class CreditFields(models.Model):
 class BankList(models.Model):
     bank_name = models.CharField(max_length=100, null=True, blank=True)
     bank_email = models.EmailField(null=True, blank=True)
-    credit_type = models.ForeignKey(CreditFields,on_delete=models.CASCADE,null=True,blank=True)
-    interest = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100)], default=0.0,null=True,blank=True)
+    credit_type = models.ForeignKey(CreditFields, on_delete=models.CASCADE, null=True, blank=True)
+    interest = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(100)], default=0.0, null=True,
+                                 blank=True)
 
     def __str__(self):
         return self.bank_name
@@ -30,10 +31,25 @@ class BlankCredit(models.Model):
     amount = models.IntegerField()
     credit_type = models.CharField(max_length=100, null=True, blank=True)
     date = models.DateTimeField(auto_now=True, auto_created=True)
-    send_user_num = models.CharField(max_length=255,null=True,blank=True,default='0')
+    send_user_num = models.CharField(max_length=255, null=True, blank=True, default='0')
     otp_status = models.BooleanField("otp status", default=False)
     bank_id = models.ForeignKey(BankList, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name_surname} | Credit Type : {self.credit_type} | User Number : {self.send_user_num}'
 
+
+class SendedBlank(models.Model):
+
+    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True, blank=True)
+    name_surname = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    amount = models.IntegerField()
+    credit_type = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now=True, auto_created=True)
+    send_user_num = models.CharField(max_length=255, null=True, blank=True, default='0')
+    otp_status = models.BooleanField("otp status", default=False)
+    bank_id = models.ForeignKey(BankList, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name_surname} | Credit Type : {self.credit_type} | User Number : {self.send_user_num}'
